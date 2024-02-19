075a2c8708d0e5a367f191938a36823d1819f9e8
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-02-19 13:37:58 +0200

    ListRowItem View added with environemnt and observed object to toggle changes

---
+++ Devote/Style/CheckBoxStyle.swift
@@ -0,0 +1,19 @@
+//
+//  CheckBoxStyle.swift
+//  Devote
+//
+//  Created by Y K on 19.02.2024.
+//
+
+import SwiftUI
+
+struct CheckBoxStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Toggle("Placeholder label", isOn: .constant(true))
+        .previewLayout(.sizeThatFits)
+}
---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,32 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by Y K on 19.02.2024.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item // (CRUD* - are the basic operator of persistent storage) create, read, update, delete
+    
+    var body: some View {
+        Toggle(isOn: $item.completion, label: {
+            Text(item.task ?? "")
+                .font(.system(size: 22, weight: .heavy, design: .rounded))
+                .foregroundStyle(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default, value: item.completion)
+        })
+        .onReceive(item.objectWillChange, perform: { _ in // publisher
+            if self.viewContext.hasChanges { // action (parameter)
+                try? self.viewContext.save()
+            }
+        })
+    }
+}
+
+//#Preview {
+//    ListRowItemView()
+//}
