b35bde498033940c0261174a609ba0149bcbffe0
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-02-19 21:39:56 +0200

    imported WidgetKit various screens size added Environemnt and adjustable Font added Proj and widget finished 🍎 🚀

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Y K on 19.02.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+        DevoteWidgetLiveActivity()
+    }
+}
