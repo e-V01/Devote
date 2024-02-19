50c9f6f5015f86dbb8f44d1f05fe3bffb9bc3245
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-02-19 13:13:12 +0200

    DarkMode for newTask item managed

--- Devote/View/NewTaskItemView.swift
+++ Devote/View/NewTaskItemView.swift
@@ -12,6 +12,7 @@ struct NewTaskItemView: View {
     @Environment(\.managedObjectContext) private var viewContext
     @State private var task: String = ""
     @Binding var isShowing: Bool
+    @AppStorage ("isDarkMode") private var isDarkMode: Bool = false
     
     private var isButtonDisabled: Bool { //checks whether textField isEmpty (Bool)
         task.isEmpty
@@ -47,7 +48,7 @@ struct NewTaskItemView: View {
                     .foregroundStyle(.pink)
                     .font(.system(size: 24, weight: .bold, design: .rounded))
                     .padding()
-                    .background(Color(UIColor.systemGray6))
+                    .background(isDarkMode ? Color(UIColor.tertiarySystemBackground) : Color(UIColor.secondarySystemBackground))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                 
                 Button {
@@ -66,7 +67,7 @@ struct NewTaskItemView: View {
             }
             .padding(.horizontal)
             .padding(.vertical, 20)
-            .background(Color.white)
+            .background(isDarkMode ? Color(UIColor.secondarySystemBackground) : Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 16))
             .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
             .frame(maxWidth: 640)
